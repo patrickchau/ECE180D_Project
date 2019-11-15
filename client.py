599f85647007219eef9fe5f36905110e7c70a253
patrickchau <patrickchau59@gmail.com>
2019-11-14 18:46:47 -0800

    client skeleton

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket
 import time
 
-#get mac address
 def getMAC(interface='wlan0'):
   # Return the MAC address of the specified interface
   try:
@@ -10,12 +9,33 @@ def getMAC(interface='wlan0'):
     str = "00:00:00:00:00:00"
   return str[0:17]
 
-mac_string = 'start' + getMAC()
+def gestures(): # recognize gestures
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.4.1', 8080))
-client.send(mac_string.encode('utf-8'))
-time.sleep(5)
-client.send(mac_string.encode('utf-8'))
-client.close()
+def sendPosition(): # collect row and position 
 
+def readServer(): # take in server messages
+
+def contolLED(): # run the LED sequence based on server input
+
+def readIMU(): # take in IMU data and analyze it somehow
+
+
+def sendMAC(host, port):
+  mac_string = 'start' + getMAC()
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((host, port))
+  # delay of 5 seconds before sending first MAC address message
+  time.sleep(5)
+  client.send(mac_string.encode('utf-8'))
+
+def runClient():
+  host = "192.168.4.1"
+  port = 8080
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((host, port))
+  sendMAC(host, port) # send over the MAC address for the 
+  client.close()
+  
+
+if __name__ == "__main__":
+  runClient()
